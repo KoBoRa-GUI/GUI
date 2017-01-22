94493b4e08f12fc0bcfea9ffd239c671865dec5d
Markus Goetz <M.Goetz2@gmx.net>
2017-01-22 17:39:05 +0100

    ink link für Präsi

--- bubble/jsonDeliverer.php
+++ bubble/jsonDeliverer.php
@@ -23,7 +23,7 @@ $linkarray = array (
 "https://docs.google.com/document/d/1R1VkvcuNqAgvH4NpReUMAcaNfdp92sxxK7GkR1rTMYc/edit#heading=h.gjdgxs",
 "https://docs.google.com/spreadsheets/d/1yvQaaoON5RaQHP-X64vRi5NHW7eZgXBm07IGbOvp1hU/edit#gid=0",
 "https://docs.google.com/presentation/d/15D33bCUltMJGjnlo2bmLP2YpY5zoJFtELeUf5qVLnxM/edit#slide=id.p4",
-"https://facebook.com",																						//exp link
+"http://boxsacktest.shop/Cloud/explorer/",																						//exp link
 "https://drive.google.com/drive/folders/0B5DLVTqFPj3ZTjU0STcxX3c5RFE",		
 "https://drive.google.com/drive/folders/0B5DLVTqFPj3ZOHBNcmpYUjBhWGc",
 "https://docs.google.com/spreadsheets/d/14QGK4LAcajOP0HBXqT4_k7ArSibyWg0u_Gy0p7RbgHQ/edit"
