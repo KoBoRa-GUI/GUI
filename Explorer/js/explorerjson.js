2a9f0e1925aa97fa7b6df43eac8afa6de92c2d37
Markus Goetz <M.Goetz2@gmx.net>
2017-01-22 14:14:06 +0100

    finale version

--- Explorer/js/explorerjson.js
+++ Explorer/js/explorerjson.js
@@ -19,12 +19,43 @@
             $("#tree_container").append('<div id="tree" data-source="ajax" class="showDivs"></div>');
             starteTree();
         }
-        function fillList(){
+
+
+        var countList = 0;
+        var doScroll = 1;
+
+     function fillList(){
+
+    var date = new Date();
+    var Stunden = date.getHours();
+    var Minuten = date.getMinutes();
+    var Sekunden = date.getSeconds();
+    var Vorsek = (Sekunden < 10) ? ":0" : ":";
+    var Vormin = (Minuten < 10) ? ":0" : ":";
     
-       var link = document.getElementById("highlight");
-        var linkName =    document.getElementById("highlight").innerHTML;
-        document.getElementById("dataList").innerHTML = document.getElementById("dataList").innerHTML + "<a  href="+ link +" target='_blank'> "+ linkName +" </a><br>";
+        var linkArray = document.getElementsByClassName("highlight");
+     
+           for(var i = 0; i < linkArray.length; i++ ){
+
+             
+               var link = linkArray[i];
+               var linkName =   linkArray[i].innerHTML; 
+               document.getElementById("dataList").innerHTML = document.getElementById("dataList").innerHTML + "<a  id='listPoint"+countList+"' href="+ link +" target='_blank'> "+ linkName +" " + Stunden + Vormin + Minuten + Vorsek + Sekunden + " Uhr </a><br>";
+
+           
+
+               
+            if(doScroll == 1){
+	       //autoscroll der Liste
+	       var elemListPoint = document.getElementById('listPoint'+countList);
+	       elemListPoint.scrollIntoView(true);
+           }
+
+           countList++;
+           }
         }
+
+
         var myInterval;
         var myInterval2;
         function starteAnwendung(){
@@ -44,8 +75,16 @@ function stoppeAnwendung() {
                     window.document.getElementById("startButton").onclick = function () {
                         starteAnwendung();
                     }
-                window.document.getElementById("stopButton").onclick = function () {
+                    window.document.getElementById("stopButton").onclick = function () {
                         stoppeAnwendung();
                     }
+                    window.document.getElementById("dataList").onmouseover = function () {
+                        doScroll = 0;
+                    }
+                    window.document.getElementById("dataList").onmouseleave = function () {
+                        doScroll = 1;
+                    }
+
+
             }
         window.onload = init;
